fe03641a8eefbb18aa13fb185a9f500c6031724b
StephenRM <stevenrm09@gmail.com>
2025-07-25 12:27:31 -0400

    init py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Steven Ricnon",                         # Tu nombre
+    author_email="stevenrm09@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/StephenRM-Dr/Repo1#",     # URL del proyecto
+)
